c2871af3ee8171466abd6aec15421cc99815abe7
Anushka Verma <130084364+AnuV1905@users.noreply.github.com>
2024-06-15 12:33:04 +0000

    modify pics

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,9 +16,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/150543-abstract-brown-and-white-diagonal-shiny-lines-background-vector-image.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "brown and white abstract background";
 
 /**
  * Sort description that expands on your title on the Home component.
