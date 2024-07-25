efde1b3c548ca3b13903b595b2dfde6a771fe00a
Anushka Verma <130084364+AnuV1905@users.noreply.github.com>
2024-07-25 01:44:59 +0000

    Updating bio and creds

--- src/App.jsx
+++ src/App.jsx
@@ -25,10 +25,10 @@ import "./styles.css";
  */
 const siteProps = {
   name: "Anushka Verma",
-  title: "Web Developer & UI/UX Designer",
+  title: "Software Development enthusiast, Web Developer & UI/UX Designer",
   email: "vermaanushka1905@gmail.com",
   gitHub: "AnuV1905",
-  instagram: "",
+  instagram: "anu.__v._",
   linkedIn: "anushka-verma-av1905",
   medium: "",
   twitter: "",
